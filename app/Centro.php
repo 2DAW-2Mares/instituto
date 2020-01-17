3e149b68b3c06f75423ce7c5399d4d0b7e651877
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-01-17 09:58:18 +0000

    Relaciones del modelo Centro

--- app/Centro.php
+++ app/Centro.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Centro extends Model
 {
-    //
+    /**
+     * Get the coordinador that owns the phone.
+     */
+    public function coordinadorObject()
+    {
+        return $this->belongsTo('App\User', 'coordinador');
+    }
+
+    /**
+     * Get the Anyosescolares that owns the phone.
+     */
+    public function anyosescolares()
+    {
+        return $this->hasMany('App\Anyoescolar', 'centro');
+    }
+
 }
